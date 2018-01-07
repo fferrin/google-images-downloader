24045269d43a71a0025187946de44fa153de8a11
fucker <facundo.ferrin@gmail.com>
2018-01-07 03:53:59 -0300

    Add comments and logger

--- google-images-downloader.py
+++ google-images-downloader.py
@@ -1,95 +1,162 @@
 # -*- coding: utf-8 -*-
 
 import json
-import urllib
+import logging
 import requests
 import os
 
 from bs4 import BeautifulSoup
+from time import localtime, strftime
 
 
 # TODO:
 # - Agregar carpeta de destino
 # - Agregar única extensión o que elija por default de la lista
-# - Si está ese nombre, aumentar contador y guardar
 # - Agregar tiempo total
-# - Agregar opción de archivo de error
+# - Agregar opción de archivo de error para borrarlos (ver cómo validarlos)
 # - Agregar formato para el nombre
 # - Límite de imágenes
 
 
-try:
-    # For Python 3.0 and later
-    from urllib.request import urlopen
-except ImportError:
-    # Fall back to Python 2's urllib2
-    from urllib2 import urlopen
+class GoogleImagesScraper:
 
-
-class Scraper:
-
-    def __init__(self):
+    def __init__(self,
+                 exts=['jpg', 'jpeg', 'png', 'bmp'],
+                 output_dir=os.path.expanduser('~'),
+                 prefix='',
+                 sufix='',
+                 limit=100,
+                 logger=False):
+        # We will make (maybe) several request to the same host, so reusing the same TCP connection
+        # result in a significant performance increase
         self.session = requests.Session()
-        self.headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36'}
+        self.session.headers.update(
+            {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36'}
+        )
+
+        # Google Image search default parameters
         self.url = 'https://www.google.com.ar/search?'
         self.params = {'tbm': 'isch'}
-        self.output_dir = os.path.expanduser('~')
-        self.allowed_extensions = ['jpg', 'jpeg', 'png', 'bmp']
+        self.allowed_exts = exts
+
+        # By default, save images and log file in a folder inside home directory
+        self.output_dir = output_dir
+
+        # Default prefix and sufix for output file names
+        self.prefix = prefix if prefix == '' else prefix + '-'
+        self.sufix = sufix if sufix == '' else '-' + sufix
+
+        # Downloads limit
+        self.limit = limit
+
+        # Number of files in output_dir
+        self.counter = 0
+
+        # Set logger
+        self._set_logger(logger)
+
         # Turn off SSL warnings
         requests.packages.urllib3.disable_warnings()
 
+    def _set_logger(self, has_logger):
+        if has_logger:
+            logging.basicConfig(
+                level=logging.INFO,
+                format='%(asctime)s [%(levelname)-8s] %(message)s',
+                datefmt='%a, %d %b %Y %H:%M:%S',
+                filename=os.path.join(self.output_dir, strftime('%Y%m%d_%H%M%S', localtime()) + '.log'),
+                filemode='a')
+            self.logger = logging.getLogger('logger')
+        else:
+            self.logger = logging.getLogger('None')
+
     def _make_soup(self):
+        self.logger.debug('Making URL')
+        # Create the URL
         url = self.url
         for k, v in self.params.items():
             url += '%s=%s&' % (k, v)
 
-        request = urllib.request.Request(url, headers=self.headers)
-        response = urllib.request.urlopen(request)
-        # Open URL and read until EOF
-        html = response.read()
+        self.logger.debug('Getting URL content')
+        # Get the URL content and convert to a tree
+        request = self.session.get(url)
+        html = request.content
 
+        self.logger.debug('Returning BeautifulSoup tree')
         return BeautifulSoup(html, "lxml")
 
-    def download_image(self, image_url):
+    def _download_image(self, image_url):
+        self.logger.debug('Getting image URL')
+        # Get image extension
         extension = image_url.split('.')[-1]
 
-        if extension not in self.allowed_extensions:
+        self.logger.debug('Checking if image extension is allowed')
+        # If image extension is not in the allowed extensions, return
+        if extension.lower() not in self.allowed_exts:
+            self.logger.debug('Image extension not allowed. Returning...')
             return
 
-        count_files = len(os.listdir(self.output_dir))
+        self.logger.debug('Setting image filename')
+        # Set local filename and update counter attribute
+        local_filename = os.path.join(self.output_dir, '%s%04d%s.%s' % (self.prefix, self.counter + 1, self.sufix, extension))
+        self.counter += 1
 
-        local_filename = os.path.join(self.output_dir, '%05d.%s' % (count_files + 1, extension))
-        print("Downloading '%s' as '%s" % (image_url, local_filename))
-
+        self.logger.info('"%s" image downloaded from "%s"' % (local_filename, image_url))
+        # Get image bytes and save them in `local_filename`
         try:
             r = self.session.get(image_url, stream=True, verify=False)
             with open(local_filename, 'wb') as f:
                 for chunk in r.iter_content(chunk_size=1024):
                     f.write(chunk)
         except ConnectionError as e:
-            print("An error has ocurred: %s" % str(e))
+            self.logger.error('An error has ocurred: %s' % str(e))
 
     def get_images(self, query, output_dir=None):
-
+        images_down = 0
+        self.logger.debug('Setting output directory')
+        # If output path is given, set attribute output_dir
+        # If not, save images in home directory + query string with underscores
         if output_dir is not None:
             self.output_dir = output_dir
         else:
             self.output_dir = os.path.join(self.output_dir, query.replace(' ', '_'))
 
         if not os.path.exists(self.output_dir):
-            # TODO: Ver si hay que agarrar alguna Exception
-            os.makedirs(self.output_dir)
+            try:
+                os.makedirs(self.output_dir)
+            except OSError as e:
+                self.logger.info('The directory "%s" already exists.' % self.output_dir)
+            else:
+                self.logger.info('"%s" directory created' % self.output_dir)
 
+        # The counter attribute, used to name image files
+        self.counter = len(os.listdir(self.output_dir))
+
+        # Set query param to use in URL
         self.params['q'] = query.replace(' ', '%20')
 
+        # Make soup and find all image divs
         soup = self._make_soup()
-        topics = soup.findAll('div', {'class': 'rg_meta'})
+        image_divs = soup.findAll('div', {'class': 'rg_meta'})
 
-        for topic in topics:
-            self.download_image(json.loads(topic.contents[0])['ou'])
+        self.logger.debug('Traversing image divs to find image URLs')
+        # For each div, create a JSON object and get 'ou' value, which is the image URL and download it
+        for div in image_divs:
+            if images_down < self.limit:
+                self._download_image(json.loads(div.contents[0])['ou'])
+                images_down += 1
+            else:
+                self.logger.debug('Image limit exceeded. Return images downloaded')
+                self.logger.info('%d images downloaded' % images_down)
+                return images_down
+
+        self.logger.info('%d images downloaded' % images_down)
+        return images_down
 
 
 if __name__ == '__main__':
-    scraper = Scraper()
+    scraper = GoogleImagesScraper(limit=1)
 
-    scraper.get_images('spacex wallpaper')
+    count = scraper.get_images('spacex wallpaper')
+    print('%d images downloaded' % count)
+
